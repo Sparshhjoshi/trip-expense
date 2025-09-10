7a4a6016510cf5f3d52cb86971f46b8148734071
sparsh joshi <sparshjoshi@sparshs-MacBook-Air-2.local>
2025-09-10 19:18:40 +0530

    Prepare for deployment on Vercel

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -65,4 +66,5 @@ def index():
     return render_template("index.html", result=result, settlements=settlements)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
